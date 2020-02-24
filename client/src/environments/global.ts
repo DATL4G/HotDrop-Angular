7d8ab738ed8811c8ea4e5321f9e1191607d1440a
DATL4G <jeff.retz08@gmail.com>
2020-02-24 17:15:36 +0100

    updated used dependencies

--- client/src/environments/global.ts
+++ client/src/environments/global.ts
@@ -12,23 +12,17 @@ export const settingsOptions: Array<String> = [
 export const dependencies: Array<String> = [
     'Angular',
     'FontAwesome',
-    'Bootstrap',
-    'NG-Bootstrap',
     'GSAP',
     'jQuery',
     'Socket.IO',
-    'Material-Components',
     'NGX-Device-Detector',
 ];
 
 export const dependencyLinks: Array<string> = [
     'https://angular.io',
     'https://fontawesome.com',
-    'https://getbootstrap.com',
-    'https://ng-bootstrap.github.io',
     'https://greensock.com/gsap/',
     'https://jquery.com',
     'https://socket.io',
-    'https://material.io/develop/web/',
     'https://koderlabs.github.io/ngx-device-detector/',
 ];
