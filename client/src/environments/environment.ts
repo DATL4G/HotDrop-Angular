aabe03e2e16af37ca459882f93c7ab42f9ccb3ba
DATL4G <jeff.retz08@gmail.com>
2020-05-15 19:36:35 +0200

    updated dependencies

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   protocol: 'http',
   websocketProtocol: 'ws',
-  serverUri: 'linux-datlag:8080',
+  serverUri: 'localhost:8080',
   rtcConfig: {
     iceServers: [{
       urls: [
